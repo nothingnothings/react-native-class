23049aaf9c515b84496357fc6c59a8de11abc6b1
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2025-04-20 17:31:29 -0300

    finished lesson 15

--- MODULO-4-BUILDING-REAL-APP/newApp/app/_layout.tsx
+++ MODULO-4-BUILDING-REAL-APP/newApp/app/_layout.tsx
@@ -9,11 +9,33 @@ import StartGameScreen from '@/screens/StartGameScreen';
 import GameScreen from '@/screens/GameScreen';
 import React from 'react';
 
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('../assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function RootLayout() {
   const [userNumber, setUserNumber] = React.useState(0);
+
+  const [dataLoaded, setDataLoaded] = React.useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 
   const startGameHandler = (selectedNumber: number) => {
     setUserNumber(selectedNumber);

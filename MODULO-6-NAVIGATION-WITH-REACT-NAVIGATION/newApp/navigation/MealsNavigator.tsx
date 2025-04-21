71c601eaed2882ff036347a04f73567ada511cdc
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2025-04-21 16:27:33 -0300

    finished lesson 14

--- MODULO-6-NAVIGATION-WITH-REACT-NAVIGATION/newApp/navigation/MealsNavigator.tsx
+++ MODULO-6-NAVIGATION-WITH-REACT-NAVIGATION/newApp/navigation/MealsNavigator.tsx
@@ -4,13 +4,32 @@ import { createAppContainer } from 'react-navigation';
 import CategoriesScreen from '@/app/CategoriesScreen';
 import CategoryMealsScreen from '@/app/CategoryMeals';
 import MealDetailScreen from '@/app/MealDetailScreen';
+import { Colors } from '@/constants/Colors';
+import { Platform } from 'react-native';
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealsScreen, // te deixa adicioar configurações extras.
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: {
+      screen: CategoryMealsScreen, // te deixa adicioar configurações extras.
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: Colors.dark,
+          borderBottomColor: Colors.dark,
+        },
+      },
+    },
+    MealDetail: MealDetailScreen,
   },
-  MealDetail: MealDetailScreen,
-});
+  {
+    // this is the stackConfig (things like header, headerTitle, etc.)
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? '#3b3b3b' : '#2b2b2b',
+        borderBottomColor: Platform.OS === 'android' ? 'white' : 'black',
+      },
+    },
+  }
+);
 
 export default createAppContainer(MealsNavigator); /// Basic setup.

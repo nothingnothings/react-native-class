3aafa1ec92ed66d689e4d13a7772c8127bf6a791
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2025-04-21 12:21:14 -0300

    finished lesson 3, module 6

---
+++ MODULO-6-NAVIGATION-WITH-REACT-NAVIGATION/newApp/app/CategoryMeals.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Colors from '@/constants/Colors';
+
+type CategoriesScreenProps = {
+  onSelectCategory: (category: string) => void;
+};
+
+const CategoryMealsScreen = (props: CategoriesScreenProps) => {
+  return <View style={styles.screen}></View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+    fontFamily: 'open-sans-bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 20,
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+});
+
+export default CategoryMealsScreen;
